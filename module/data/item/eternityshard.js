4ede5f872b554a7c4d897cec126a4d49b936ea7d
farling42 <github@amusingtime.uk>
2026-01-27 11:01:02 +0000

    Change Eternity Shard to have value/max for possibilities

--- module/data/item/eternityshard.js
+++ module/data/item/eternityshard.js
@@ -11,11 +11,24 @@ export class EternityShardItemData extends BaseItemData {
   static defineSchema() {
     return {
       ...super.defineSchema('eternityshard'),
-      possibilities: new fields.NumberField({ initial: 3, integer: true }),
+      possibilities: new fields.SchemaField({
+        max: new fields.NumberField({ initial: 3, integer: true, nullable: false }),
+        value: new fields.NumberField({ initial: 3, integer: true, nullable: false }),
+      }),
       powers: new fields.StringField({ initial: '' }),
       purpose: new fields.StringField({ initial: '' }),
       restrictions: new fields.StringField({ initial: '' }),
       tappingDifficulty: new fields.NumberField({ initial: 18, integer: true }),
     };
+  }
+
+  static migrateData(data) {
+    if (Object.hasOwn(data, "possibilities") && typeof data.possibilities === 'number') {
+      data.possibilities = {
+        value: data.possibilities,
+        max: data.possibilities,
+      }
+    }
+    return super.migrateData(data);
   }
 }
