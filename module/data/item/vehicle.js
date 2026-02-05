79a3b4c6ab4c805f1023dddb7bd9f8339c8a3b74
farling42 <github@amusingtime.uk>
2026-02-05 16:28:43 +0000

    vehicle top speed & toughness are Number not String

--- module/data/item/vehicle.js
+++ module/data/item/vehicle.js
@@ -11,11 +11,11 @@ export class VehicleItemData extends GeneralItemData {
   static defineSchema() {
     return {
       ...super.defineSchema('vehicle'),
-      mr: new fields.NumberField({ initial: -1, integer: true }),
-      pass: new fields.NumberField({ initial: 1, integer: true }),
-      topspeed: new fields.StringField({ initial: '' }),
-      tough: new fields.StringField({ initial: '' }),
-      wounds: new fields.NumberField({ initial: 3, integer: true }),
+      mr: new fields.NumberField({ initial: -1, integer: true, nullable: false }), // maneuver
+      pass: new fields.NumberField({ initial: 1, integer: true, nullable: false }), // passengers
+      topspeed: new fields.NumberField({ initial: 1, integer: true, nullable: false }),
+      tough: new fields.NumberField({ initial: 1, integer: true, nullable: false }),  // toughness
+      wounds: new fields.NumberField({ initial: 3, integer: true, nullable: false }),
     };
   }
 }
