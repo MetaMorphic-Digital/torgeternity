ddab946eb0d3fbf5b41cefc42348c1c0c9c5c93e
farling42 <github@amusingtime.uk>
2026-03-07 00:28:26 +0000

    Store statusModifier changes directly on CONFIG.statusEffects

--- module/data/actor/base.js
+++ module/data/actor/base.js
@@ -4,6 +4,10 @@ export class BaseActorData extends foundry.abstract.TypeDataModel {
   prepareBaseData() {
     super.prepareBaseData();
 
+    /**
+     * Actual modifiers from the various game statuses
+     * @public
+     */
     this.statusModifiers = {
       stymied: 0,
       vulnerable: 0,
@@ -12,10 +16,18 @@ export class BaseActorData extends foundry.abstract.TypeDataModel {
       concentrating: 0,
     };
 
+    /** 
+     * How this Actor modifies the statusModifiers of the target.
+     * @public
+     */
     this.targetModifiers = {
-      darkness: 0,
+      darkness: 0,    // e.g. For Darkvision, this should be 4
     };
 
+    /**
+     * The various defensive values on this Actor.
+     * @public
+     */
     this.defenses = {
       damageTraits: {
         // Armor: addition armor of the defender when damage is of the indicated type
@@ -32,32 +44,21 @@ export class BaseActorData extends foundry.abstract.TypeDataModel {
         lightningDefense: 0
       }
     }
-
-    // Make visible directly from Actor.
-    /*const actor = this.parent;
-    actor.statusModifiers = this.statusModifiers;
-    actor.targetModifiers = this.targetModifiers;
-    actor.defenses = this.defenses;*/
   }
 
   prepareDerivedData() {
     super.prepareDerivedData();
-    // Here Effects are applied, whatever follow cannot be directly affected by Effects
-    const actor = this.parent;
-    const statuses = actor.statuses;
 
-    // apply status effects (note that base values of 0 might have been modified by Active Effects)
-    this.statusModifiers.stymied += statuses.has('veryStymied') ? -4 : statuses.has('stymied') ? -2 : 0;
-    this.statusModifiers.vulnerable += statuses.has('veryVulnerable') ? 4 : statuses.has('vulnerable') ? 2 : 0;
-    this.statusModifiers.darkness += statuses.has('pitchBlack') ? -6 : statuses.has('dark') ? -4 : statuses.has('dim') ? -2 : 0;
-    this.statusModifiers.waiting += statuses.has('waiting') ? -2 : 0;
-    this.statusModifiers.concentrating += actor.appliedEffects.filter(ef => ef.statuses.has('concentrating')).length * -2;
+    // CONFIG.statusEffects has the relevant changes to this.statusModifiers which will have been applied.
+    // (In Foundry V14, other AE might want to use the numeric modifier of the effect as the value of another effect.)
 
     // Place limits on the modifiers (can't cross the 0 boundary)
+    // NEGATIVE modifiers
     if (this.statusModifiers.stymied > 0) this.statusModifiers.stymied = 0;
     if (this.statusModifiers.darkness > 0) this.statusModifiers.darkness = 0;
     if (this.statusModifiers.waiting > 0) this.statusModifiers.waiting = 0;
     if (this.statusModifiers.concentrating > 0) this.statusModifiers.concentrating = 0;
+    // POSITIVE modifiers
     if (this.statusModifiers.vulnerable < 0) this.statusModifiers.vulnerable = 0;
   }
 
